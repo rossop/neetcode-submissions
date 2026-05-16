8158e04371ecd83fdf40d430abd5281edf912369
Rosso <25611971+rossop@users.noreply.github.com>
2026-05-16 14:09:30 +0200

    Add: eating-bananas - submission-6

---
+++ Data Structures & Algorithms/eating-bananas/submission-6.go
@@ -0,0 +1,29 @@
+func calcHours(piles []int, k int) int {
+    totalHours := 0
+    for _, p := range piles {
+        totalHours += (p + k - 1) / k
+    }
+    return totalHours
+}
+
+func minEatingSpeed(piles []int, h int) int {
+    maxPile := 0
+    for _, p := range piles {
+        if p > maxPile {
+            maxPile = p
+        }
+    }
+    left, right := 1, maxPile
+    res := right
+
+    for left <= right {
+        mid := left + (right - left) / 2
+        if calcHours(piles, mid) <= h {
+            res = mid
+            right = mid - 1
+        } else {
+            left = mid + 1
+        }
+    }
+    return res
+}
