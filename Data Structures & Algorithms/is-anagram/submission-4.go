6ca075397c0eca3291860dca5e7795229e560c5c
Rosso <25611971+rossop@users.noreply.github.com>
2026-05-21 01:17:06 +0200

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.go
@@ -0,0 +1,34 @@
+import "slices"
+
+func isAnagramSort(s string, t string) bool {
+    if len(s) != len(t) {
+        return false
+    }
+    sRunes := []rune(s)
+    tRunes := []rune(t)
+
+    slices.Sort(sRunes)
+    slices.Sort(tRunes)
+    
+    return slices.Equal(sRunes, tRunes)
+}
+
+func isAnagram(s string, t string) bool {
+    if len(s) != len(t) {
+        return false
+    }
+
+    counts := make(map[rune]int)
+
+    for _, char := range s {
+        counts[char]++
+    }
+
+    for _, char := range t {
+        counts[char]--
+        if counts[char] < 0 {
+            return false
+        }
+    }
+    return true
+}
