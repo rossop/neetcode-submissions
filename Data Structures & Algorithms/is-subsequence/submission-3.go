6b177288db72dbcfc69a2856ac16995bf9d38012
Rosso <25611971+rossop@users.noreply.github.com>
2026-05-21 19:27:51 +0200

    Add: is-subsequence - submission-3

---
+++ Data Structures & Algorithms/is-subsequence/submission-3.go
@@ -0,0 +1,10 @@
+func isSubsequence(s string, t string) bool {
+	sPtr, tPtr := 0, 0
+	for sPtr < len(s) && tPtr < len(t) {
+		if s[sPtr] == t[tPtr] {
+			sPtr++
+		} 
+		tPtr++
+	}
+	return sPtr == len(s) 
+}
